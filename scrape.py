efc36b816d9b3a1d42f60055a2c107c38c767cc7
mahmudhasankhan <khanzadeh008@gmail.com>
2024-11-07 17:36:48 +0600

    now scrape.py saves data as csv rather than individual jsons

--- scrape.py
+++ scrape.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
-import json
+# import json
 import argparse
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--url",
@@ -19,7 +20,7 @@ else:
     driver = webdriver.Chrome()
     driver.get(URL)
 
-time.sleep(8)
+time.sleep(4)
 
 soup = BeautifulSoup(driver.page_source, "html.parser")
 special_divs = soup.find_all(
@@ -28,16 +29,21 @@ special_divs = soup.find_all(
 item_links = []
 for div in special_divs:
     for item in div.find_all('a', attrs={"class": "details-link"}):
-       item_links.append(item['href'])
+        item_links.append(item['href'])
 
 driver.quit()
 
 product_driver = webdriver.Chrome()
 
+data = {
+    "product link": [],
+    "product details": [],
+}
+
 for i, link in enumerate(item_links):
     # if i == 3:
     #     break
-    
+
     product_link = "https://sindabad.com" + link
     product_driver.get(product_link)
     time.sleep(8)
@@ -45,12 +51,21 @@ for i, link in enumerate(item_links):
     product_desc = new_soup.find(
         "div", {"class": "osahan-main-body py-3 py-md-4"})
     print(f"Product Number: {i+1}")
-    
-    text_list = []
-    text_list.append(f"Product link: {product_link}")
-    
+
+    data["product link"].append(product_link)
+    texts = ""
     for li in product_desc.find_all("li"):
-        text_list.append(li.text)
-    with open(f"./data/product_id_{i+1}.json", "w") as f:
-        json.dump(text_list, f)
+        if li.text != "":
+            texts += li.text + ", "
+    # text_list["product details"].append([
+    #     li.text for li in product_desc.find_all("li") if li.text != ""])
+
+    data["product details"].append(texts)
+
+# print(data["product details"])
+# with open("./data/product_list.json", "w") as f:
+#     json.dump(data, f)
+
+dataframe = pd.DataFrame.from_dict(data)
+dataframe.to_csv("./data/product_list.csv", header=True, index=False)
 product_driver.quit()
